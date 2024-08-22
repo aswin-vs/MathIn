0f977b82b692e35b641f18b23dfce17135fce275
Aswin V S <68040205+aswin-vs@users.noreply.github.com>
2024-08-22 14:55:10 +0530

    Content update2

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 import Footer from './Footer';
 import '../styles/Content.css';
 
-// import M1 from '../assets/MathIn1.svg';
-// import M2 from '../assets/MathIn2.svg';
-// import M3 from '../assets/MathIn3.svg';
+import M1 from '../assets/MathIn1.svg';
+import M2 from '../assets/MathIn2.svg';
+import M3 from '../assets/MathIn3.svg';
 import closeIcon from '../assets/xMark.svg';
 import ContentPdf from '../assets/MathIn.pdf';
 
@@ -20,20 +20,29 @@ const Content = ({ isVisible, onClose }) => {
           <img src={closeIcon} alt="CloseIcon" />
         </button>
 
-        {/* <div className='content-box3'>
+        <div className='content-box3'>
           <img src={M1} alt="M1" />
           <img src={M2} alt="M2" />
           <img src={M3} alt="M3" />
-        </div> */}
+        </div>
 
         {/* <embed src={ContentPdf} type="application/pdf" height="450" width="1150" /> */}
-        <iframe
+        {/* <iframe
           src={ContentPdf}
           type="application/pdf"
           width="1150px"
           height="450px"
           style={{ border: 'none' }}
-        />
+        /> */}
+
+        {/* <iframe
+          src={`https://drive.google.com/file/d/1xmoqKM_aR8rKm3mBI-M3j8OujBf9z1uP/preview`}
+          width="1150px"
+          height="450px"
+          style={{ border: 'none', transform: 'scale(1)', transformOrigin: '0 0' }}
+          allowFullScreen
+        /> */}
+
       </div>
       <Footer />
     </div>
