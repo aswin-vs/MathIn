017a6dd32dc299c41b8adf860d99e2ecf12f70cc
Aswin V S <68040205+aswin-vs@users.noreply.github.com>
2024-12-23 21:03:55 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <DisableNavigations />
       <App />
     </HashRouter>
