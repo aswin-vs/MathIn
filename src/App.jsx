4337002a8939e461726355b6f937b8c7bfb592e7
Aswin V S <68040205+aswin-vs@users.noreply.github.com>
2024-12-23 20:56:38 +0530

    router fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // App.jsx
 // This project is developed and maintained by 'Aswin V S' (https://github.com/aswin-vs)
 
-import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Homepage from './components/homepage';
 import Prepare from './components/prepare';
@@ -11,14 +12,16 @@ import CertificateVerify from "./components/certificateVerify";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="prepare" element={<Prepare />} />
-      <Route path="practice" element={<Practice0 />} />
-      <Route path="participate" element={<Participate0 />} />
-      <Route path="verify/:certificateId" element={<CertificateVerify />} />
-      <Route path="*" element={<Navigate to="/" replace={true} />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="prepare" element={<Prepare />} />
+        <Route path="practice" element={<Practice0 />} />
+        <Route path="participate" element={<Participate0 />} />
+        <Route path="verify/:certificateId" element={<CertificateVerify />} />
+        <Route path="*" element={<Navigate to="/" replace={true} />} />
+      </Routes>
+    </Router>
   );
 }
 
