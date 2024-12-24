99ce5f5874c1be6a2074baf66f343780a2cc12f3
Aswin V S <68040205+aswin-vs@users.noreply.github.com>
2024-12-24 08:00:01 +0530

    rebounce

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 // This project is developed and maintained by 'Aswin V S' (https://github.com/aswin-vs)
 
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom'; // Import HashRouter
 
 import Homepage from './components/homepage';
 import Prepare from './components/prepare';
@@ -13,25 +12,14 @@ import CertificateVerify from "./components/certificateVerify";
 const App = () => {
   return (
     <Routes>
-      {/* Regular BrowserRouter routes */}
       <Route path="/" element={<Homepage />} />
       <Route path="prepare" element={<Prepare />} />
       <Route path="practice" element={<Practice0 />} />
       <Route path="participate" element={<Participate0 />} />
-
-      {/* For /verify, use HashRouter */}
-      <Route path="verify/*" element={
-        <HashRouter> {/* Use HashRouter for /verify route */}
-          <Routes>
-            <Route path=":certificateId" element={<CertificateVerify />} />
-          </Routes>
-        </HashRouter>
-      } />
-
-      {/* Redirect to homepage if no match */}
+      <Route path="verify/:certificateId" element={<CertificateVerify />} />
       <Route path="*" element={<Navigate to="/" replace={true} />} />
     </Routes>
   );
-};
+}
 
 export default App;
